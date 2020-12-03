29cfc4ce010119358e0eaeaca5292bf508d7e10a
saurav188 <sauravthakur188@gmail.com>
2020-12-03 13:00:38 +0545

    added maze generating algorithm

--- src/components/maze.js
+++ src/components/maze.js
@@ -1,14 +1,29 @@
 import '../css/maze.css'
+import React, { useState } from 'react';
 
 function Maze() {
-    var no_rows=new Array(20).fill(true);
-    var no_columns=new Array(30).fill(true);
-    var i=1;
+    var no_rows=21;
+    var no_columns=31;
+    var rows=new Array(no_rows).fill(true);
+    var columns=new Array(no_columns).fill(true);
+    const [clicked,change_clicked]=useState(false)
+    function build_maze(event){
+        if(document.getElementById('drawer').checked === true){
+            event.target.classList.add('obstacle')
+        }else if(document.getElementById('eraser').checked === true){
+            if(event.target.classList.contains('obstacle')){
+                event.target.classList.remove('obstacle')
+            };
+        };
+    };
     return (
         <div className="maze">
-           {no_rows.map(()=>
-                no_columns.map(()=>
-                <div className='grid'></div>
+           {rows.map(()=>
+                columns.map(()=>
+                    <div className='grid' 
+                        onClick={event=>{change_clicked(!clicked);build_maze(event);}} 
+                        onMouseOver={event=>{if(clicked){build_maze(event)}}}>
+                    </div>
                 )
             )}
         </div>
--- src/components/maze_controls.js
+++ src/components/maze_controls.js
@@ -1,6 +1,8 @@
 import '../css/maze_control.css'
 
 function Maze_control(){
+    var no_rows=21;
+    var no_columns=31;
     function choose_eraser(event){
         document.getElementById('eraser').checked = true;
         document.getElementById('drawer').checked = false;
@@ -8,6 +10,88 @@ function Maze_control(){
     function choose_drawer(event){
         document.getElementById('eraser').checked = false;
         document.getElementById('drawer').checked = true;
+    };
+    function getRandomInt(min, max) {
+        if(min===max){return min}
+        var min = Math.ceil(min);
+        var max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    function get_next(current,visited){
+        var neighbours=[];
+        var x=current[0]
+        var y=current[1]
+        visited[`${x},${y}`]=true;
+        //top neighbour
+        if(y>=3 && visited[`${x},${y-2}`]===undefined){
+            neighbours.push([x,(y-2)])
+        };
+        //right neighbour
+        if(x<=(no_columns-2) && visited[`${x+2},${y}`]===undefined){
+            neighbours.push([(x+2),y])
+        };
+        //bottom neighbour
+        if(y<=(no_rows-2) && visited[`${x},${y+2}`]===undefined){
+            neighbours.push([x,(y+2)])
+        };
+        //left neighbour
+        if(x>=3 && visited[`${x-2},${y}`]===undefined){
+            neighbours.push([(x-2),y])
+        };
+        var no_neighbours=neighbours.length;
+        if(no_neighbours===0){
+            return 0;
+        };
+        var result=neighbours[getRandomInt(1,no_neighbours)-1]
+        return result
+    };
+    function maze_generator(e){
+        const grids=Array.from(document.getElementsByClassName('grid'));
+        grids.forEach(each=>{
+            if(each.classList.contains('obstacle')){
+                each.classList.remove('obstacle');
+            };
+        });
+        for(var i=0;i<grids.length;i++){
+            var y=Math.floor(i/no_columns)+1;
+            var x=(i%no_columns)+1;
+            if(x%2===0 || y%2===0){
+                grids[i].classList.add('obstacle');
+            };
+        };
+        var stack=[[1,1]];
+        var visited={}
+        var current=[1,1]
+        while(stack.length>0){
+            var next=get_next(current,visited);
+            if(next===0){current=stack.pop();continue};
+            var index=(((current[1]-1)*no_columns)+(current[0]-1))
+            //if current and next is in same row
+            if(current[1]===next[1]){
+                //left side
+                if(current[0]>next[0]){
+                        grids[index-1].classList.remove('obstacle');
+                }
+                //right side
+                else if(current[0]<next[0]){
+                    grids[index+1].classList.remove('obstacle');
+                };
+            }
+            //if current and next is in same column
+            else if(current[0]===next[0]){
+                //top
+                if(current[1]>next[1]){
+                    grids[index-no_columns].classList.remove('obstacle');
+                }
+                //bottom
+                else if(current[1]<next[1]){
+                    grids[index+no_columns].classList.remove('obstacle')
+                };
+            };
+            setTimeout(500)
+            stack.push(next);
+            current=next;
+        };
     };
     return (
         <div className="maze_control">
@@ -21,7 +105,7 @@ function Maze_control(){
             <lable for="eraser">eraser</lable>
             <input type="checkbox" id="drawer" class="checkbox" onClick={choose_drawer}></input>
             <lable for="drawer">drawer</lable>
-            <button class="btn generate-maze" onClick={()=>alert('hi')}>Generate maze</button>
+            <button class="btn generate-maze" onClick={e=>maze_generator(e)}>Generate maze</button>
         </div>
     )
 };
