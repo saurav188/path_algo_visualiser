a4414eea0bb0c5fcf74fc88908afcd34e966ea9f
saurav188 <sauravthakur188@gmail.com>
2020-12-10 17:10:10 +0545

    added title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Maze_control from './components/maze_controls.js'
 import './css/index.css'
 
 function App() {
+    document.title="Path algo visualizer"
     return (
         <div className="App">
           <div className="alert">
